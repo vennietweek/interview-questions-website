529151c1c0e530db6082b59b822e86ee759914d1
Venessa Tan <venessa@Venessas-MacBook-Air.local>
2023-10-27 12:43:14 +0800

    Resolvers

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer, UserInputError } = require('apollo-server-express');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
 const { MongoClient } = require('mongodb');
+const resolvers = require('./resolvers.js');
 
 /******************************************* 
 DATABASE CONNECTION CODE
@@ -24,60 +25,6 @@ async function connectToDb() {
 /******************************************* 
 GraphQL CODE
 ********************************************/  
-const resolvers = {
-  Query: {
-    // User Service (USV) Resolvers
-    //getUserProfile: getUserProfileResolver,
-
-    // Question Service (QSV) Resolvers
-    //getAllQuestions: () => getAllQuestionsResolver,
-  },
-  Mutation: {
-    // User Service (USV) Resolvers
-    signUpUser: signUpUserResolver,
-    //updateUserProfile: updateUserProfileResolver,
-    //deregisterUser: deregisterUserResolver,
-
-    // Question Service (QSV) Resolvers
-    //addQuestion: addQuestionResolver,
-    //deleteQuestion: deleteQuestionResolver,
-    //updateQuestion: updateQuestionResolver,
-    //Question: QuestionResolver,
-  }
-};
-
-// User Service (USV) Resolvers
-async function signUpUserResolver(_, args) 
-{
-  try {
-    const { name, email, profile } = args;
-    console.log(name, email, profile);
-    // Check if the user with the provided email already exists
-    const existingUser = await db.collection('users').findOne({ email });
-    
-    if (existingUser) {
-      throw new Error('User with this email already exists.');
-    }
-
-    // Create a new user document
-    const newUser = {
-      name,
-      email,
-      profile,
-      id: await db.collection('users').countDocuments() + 1,
-    };
-
-    // Insert the new user into the "users" collection
-    const result = await db.collection('users').insertOne(newUser);
-
-    // Get the inserted user document
-    const insertedUser = result.ops[0];
-    console.log(insertedUser);
-    return insertedUser;
-  } catch (error) {
-    throw new Error(`Error signing up user: ${error.message}`);
-  }
-};
 
 
 /******************************************* 
